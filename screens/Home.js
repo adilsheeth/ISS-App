537f625d7d84cd1552a5de97075fb947a623eacd
Adil Sheeth <adilsheeth21@gmail.com>
2022-09-09 04:01:20 +1000

    Added title and buttons to home screen

--- screens/Home.js
+++ screens/Home.js
@@ -1,19 +1,70 @@
 import * as React from "react";
-import { Text, View } from "react-native";
+import { Text, View, SafeAreaView, Platform, StatusBar, TouchableOpacity } from "react-native";
 
 export default class Home extends React.Component {
     render() { 
         return (
-            <View style={{
-                flex: 1,
-                justifyContent: "center",
-                alignItems: "center",
-            }}>
-                <Text>
-                    Home Screenn.
-                </Text>
+            <View style={styles.container}>
+                <SafeAreaView style={styles.droidSafeArea} />
+                <View style={styles.titleBar}>
+                    <Text style={styles.titleText}>
+                        ISS Tracker App
+                    </Text>
+                </View>
+                <TouchableOpacity style={styles.routeCard}>
+                    <Text style={styles.routeText}>ISS Location</Text>
+                    <Text style={styles.knowMore}>{"Know More -->" }</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity style={styles.routeCard}>
+                    <Text style={styles.routeText}>Meteors</Text>
+                    <Text style={styles.knowMore}>{"Know More -->" }</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity style={styles.routeCard}>
+                    <Text style={styles.routeText}>Updates</Text>
+                    <Text style={styles.knowMore}>{"Know More -->" }</Text>
+                </TouchableOpacity>
             </View>
         );
     }
 }
  
+const styles = {
+    container: {
+        flex: 1
+    },
+    titleText: {
+        fontSize: 40,
+        fontWeight: "bold",
+        color: "white",
+    },
+    droidSafeArea: {
+        marginTop: Platform.OS == "android" ? StatusBar.currentHeight : 0,
+    },
+    titleBar: {
+        flex: 0.15,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    routeCard: {
+        flex: 0.25,
+        marginLeft: 50,
+        marginRight: 50,
+        marginTop: 50,
+        borderRadius: 30,
+        backgroundColor: "white",
+    },
+    routeText: {
+        fontSize: 35,
+        fontWeight: "bold",
+        color: "black",
+        marginTop: 75,
+        paddingLeft: 30,
+    },
+    knowMore: {
+        paddingLeft: 30,
+        color: "red",
+        fontSize: 15,
+    },
+}
