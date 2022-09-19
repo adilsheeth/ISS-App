aa5b8ca45191bcc1535f35901c6373a2f051ce1b
Adil Sheeth <adilsheeth21@gmail.com>
2022-09-19 18:01:05 +1000

    Added api for meteors

--- screens/Meteors.js
+++ screens/Meteors.js
@@ -1,18 +1,53 @@
+import axios from "axios";
 import * as React from "react";
-import { Text, View } from "react-native";
+import { Alert, Text, View } from "react-native";
 
 export default class Meteors extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      meteors: {},
+
+    }
+  }
+
+  componentDidMount() {
+    this.getMeteors();
+  }
+
+  getMeteors = () => {
+    axios
+      .get(
+        "https://api.nasa.gov/neo/rest/v1/feed?api_key=krQdOEYR874N5EZyDlVOFC2n7jVKVsUsaXCkZAHU"
+      )
+      .then((response) => {
+        this.setState({
+          meteors: response.data.near_earth_objects,
+        });
+        console.log(Object.keys(this.state.meteors));
+      })
+      .catch((error) => {
+        Alert.alert("Error", error.message);
+      });
+    
+  }
+
   render() {
     return (
-      <View
-        style={{
-          flex: 1,
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
+      <View style={styles.container} >
         <Text> Meteors Screen</Text>
       </View>
     );
   }
 }
+
+const styles = {
+  container: {
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+        }
+}
+
+//API Key = krQdOEYR874N5EZyDlVOFC2n7jVKVsUsaXCkZAHU
