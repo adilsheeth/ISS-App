14b4996d45285fa64b25a6e48c1748c7d7ab059d
Adil Sheeth <adilsheeth21@gmail.com>
2022-09-27 04:01:49 +1000

    Added flatlist and meteor images

--- screens/Meteors.js
+++ screens/Meteors.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import * as React from "react";
-import { Alert, Text, View } from "react-native";
+import { Alert, Text, View, FlatList, SafeAreaView, Platform, StatusBar } from "react-native";
 
 export default class Meteors extends React.Component {
 
@@ -32,6 +32,27 @@ export default class Meteors extends React.Component {
       })
   }
 
+  keyExtractor = (item, index) => {
+    index.toString();
+  }
+
+  renderItem = (item) => {
+    let meteor = item;
+    if(meteor.threatScore <= 30){
+      bg_img = require("../assets/meteor_bg1.png");
+      speed = require("../assets/meteor_speed1.gif");
+      size = 100;
+    } else if(meteor.threatScore <= 75){
+      bg_img = require("../assets/meteor_bg2.png");
+      speed = require("../assets/meteor_speed2.gif");
+      size = 150;
+    } else {
+      bg_img = require("../assets/meteor_bg3.png");
+      speed = require("../assets/meteor_speed3.gif");
+      size = 200;
+    }
+  }
+
   render() {
     if (Object.keys(this.state.meteors).length == 0) {
       return (
@@ -43,13 +64,30 @@ export default class Meteors extends React.Component {
       let meteorArr = Object.keys(this.state.meteors).map((meteor_date) => {
         return this.state.meteors[meteor_date];
       });
+
       let meteors = [].concat.apply([], meteorArr);
+
       meteors.forEach(function (element) {
         let diameter = (element.estimated_diameter.kilometers.estimated_diameter_min + element.estimated_diameter.kilometers.estimated_diameter_max) / 2;
         let threatScore = (diameter / element.close_approach_data[0].miss_distance.kilometers) * 1000000000;
       });
+
+      meteors.sort(function (a,b){
+        return b.threatScore - a.threatScore;
+      });
+      
+      meteors = meteors.slice(0,5);
       return (
-        <Text>Calculating Treat Score...</Text>
+        <View style={styles.container}>
+          <SafeAreaView style={styles.droidSafeArea}>
+            <FlatList 
+              data={meteors} 
+              renderItem={this.renderItem}
+              keyExtractor={this.keyExtractor}
+              horizontal={true}
+            />
+          </SafeAreaView>
+        </View>
       );
     }
   }
@@ -60,6 +98,9 @@ const styles = {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+  },
+  droidSafeArea: {
+    marginTop: Platform.OS == "android" ? StatusBar.currentHeight : 0,
   }
 }
 
